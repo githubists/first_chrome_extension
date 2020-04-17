3d0f64b023dd3039a262bfc9a4365e466d881fac
sunrise <thoftbank@gmail.com>
2020-04-17 12:19:02 +0900

    Fix セレクタの修正

--- src/removeads.js
+++ src/removeads.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var query =
-    '.ap_container, [class*="ads"], [class*="Ads"], [class*="-ad"], [class*="-Ad"], [class*="ad-"], [class*="Ad-"], [id*="ads"], [id*="Ads"], [id*="-ad"], [id*="-Ad"], [id*="ad-"], [id*="Ad-"]';
+    '[class*="Ad"], [class*="-ad"], [class*="-Ad"], [class*="ad-"], [class*="Ad-"], [id*="Ads"], [id*="-ad"], [id*="-Ad"], [id*="ad-"], [id*="Ad-"]';
 
   var element = document.querySelector(query);
 
